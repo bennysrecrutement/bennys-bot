25582c1b99079ae4d5859a1a062a9cd998620c0b
benny's ambition <joackimhurez252006@gmail.com>
2026-09-17 18:03:24 +0200

    Update index.js

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ const {
 } = require("discord.js");
 
 // --- 1) CONFIGURATION (variables d'environnement) -----------
-const TOKEN = process.env.DISCORD_TOKEN || "MTU0OTgzMjcxNzA1MzcyMjc3NA.GMtdBa.bZvwrOVay53LduEjkqlA3KP9P_F7sMvpcPihgo";
+const TOKEN = process.env.DISCORD_TOKEN || "MTU0OTgzMjcxNzA1MzcyMjc3NA.G4_EX3.BQdum5L0pVmI-1m8ZEczx3_YQzzSX91tddQjRQ";
 const GUILD_ID = process.env.GUILD_ID || "0";
 const SALON_CANDIDATURES_ID = process.env.SALON_CANDIDATURES_ID || "0";
 const SALON_LOGS_ID = process.env.SALON_LOGS_ID || "0";
